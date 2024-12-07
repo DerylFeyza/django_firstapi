a22cf0d1e4a05b9c44e878181952ae0f52036ab5
Deryl Feyza <muhammadderylfeyza@gmail.com>
2024-12-07 11:46:31 +0700

    feat: sending email

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,6 +139,14 @@ DATABASES = {
     }
 }
 
+# Celery settings
+CELERY_BROKER_URL = 'amqp://localhost'  # RabbitMQ broker URL
+CELERY_RESULT_BACKEND = 'rpc://'  # Use RPC backend for results
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -177,3 +189,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 APPEND_SLASH=False
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- user/utils.py
+++ user/utils.py
@@ -1,9 +1,24 @@
 import pyotp
 from datetime import datetime, timedelta, timezone
-from .enums import TokenTypeClass  
+from .enums import TokenTypeClass
+from typing import Dict, Any
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+
+def send_email(subject: str, recipient_list: list, html_alternative: Any, attachment: Dict = None):
+    msg = EmailMultiAlternatives(
+        subject, '', settings.EMAIL_HOST_USER, recipient_list
+    )
+    if attachment is not None:
+        msg.attach(
+            attachment.get('name'), attachment.get('file'), attachment.get('type')
+        )
+    msg.attach_alternative(html_alternative, "text/html")
+    msg.send(fail_silently=False)
 
 def generate_otp(user, token_type: TokenTypeClass):
     from .models import Token
+    from .tasks import send_user_creation_email
     totp = pyotp.TOTP(pyotp.random_base32(), interval=300)
     otp = totp.now()
     expires_at = datetime.now(timezone.utc) + timedelta(minutes=5)
@@ -15,5 +30,10 @@ def generate_otp(user, token_type: TokenTypeClass):
             'expires_at': expires_at,
             'created_at': datetime.now(timezone.utc)
         }
-    )    
-    return otp
+    )
+    user_data = {
+        "email": user.email,
+        "token": otp
+    }  
+    send_user_creation_email(user_data)
+
